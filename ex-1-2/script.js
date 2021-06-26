6a1acf6f12588406c8aae7365cc95bca0acd11b4
jamieminh <48439646+jamieminh@users.noreply.github.com>
2021-06-26 20:25:13 +0700

    Fix: Use function for each solution, add another solution for problem 2

--- ex-1-2/script.js
+++ ex-1-2/script.js
@@ -1,64 +1,118 @@
 // ---------------------------------------------------------------
 // Exercise 1: Remove duplicate from array
-const arr = [1,2,2,3,4,5,3,2,4,5,6,7,5,6] 
+const arr = [1, 2, 2, 3, 4, 5, 3, 2, 4, 5, 6, 7, 5, 6]
 
 // 1. Using Set
-const unique_1 = Array.from(new Set(arr))
-console.log("Using Set: ", unique_1);
+const removeDupOne = (arr) => {
+    return Array.from(new Set(arr))
+}
+
 
 // 2. An extra map to store visited element
-const visited = {}
-const unique_2 = []
-arr.forEach(item => {
-    if (!visited[item]) {
-        unique_2.push(item)
-        visited[item] = true
-    }
-})
-console.log("Using extra map: ", unique_2);
-// console.log("Visited: ", visited);
+const removeDupTwo = (arr) => {
+    const visited = {}
+    const unique = []
+    arr.forEach(item => {
+        if (!visited[item]) {
+            unique.push(item)
+            visited[item] = true
+        }
+    })
+    return unique
+}
 
 
 // 3. Use indexOf() to return the first index that has the value
 // Time complexity: best case O(nlog(n)) because the indexOf() stops as soon as an element is found
-const unique_3 = arr.filter((item, index) => arr.indexOf(item) === index)
-console.log("Using indexOf on Original array: ", unique_3);
+const removeDupThree = (arr) => {
+    return arr.filter((item, index) => arr.indexOf(item) === index)
+}
 
 
 // 4. Use indexOf on final array
 // worst case O(n^2) if there's little to no duplicate
-const unique_4 = []
-arr.forEach(item => {
-    if (unique_4.indexOf(item) === -1) {
-        unique_4.push(item)
-    }
-})
-console.log("Use indexOf on Final Array: ", unique_4);
+const removeDupFour = (arr) => {
+    const unique = []
+    arr.forEach(item => {
+        if (unique.indexOf(item) === -1) {
+            unique.push(item)
+        }
+    })
+    return unique
+}
+
+// console.log("Using Set: ", removeDupOne(arr));
+// console.log("Using extra map: ", removeDupTwo(arr));
+// console.log("Using indexOf on Original array: ", removeDupThree(arr));
+// console.log("Use indexOf on Final Array: ", removeDupFour(arr));
 
 
 // ---------------------------------------------------------------
 // Exercise 2: Find Repetitions
-const arr_2 = [1,4,5,7,3,1,7,5,8,3,3,9,2,3,4,4,3,2,4,4,6,7,5,6] 
+const arr_2 = [1, 4, 5, 7, 3, 1, 7, 5, 8, 3, 3, 9, 2, 3, 4, 4, 3, 2, 4, 4, 6, 7, 5, 6]
 
-const counters = {}
-arr_2.forEach(item => {
-    // add 1 if item already in counters obj, else set it to 1
-    counters[item] = counters[item] ? counters[item] += 1 : 1
-})
-
-let max = 0                         // current max repititions
-const maxValues = []                // array contains items with max repetitions
-
-for (const [key, value] of Object.entries(counters)) {
-    // if value is higher than current max
-    if (value > max) {
-        max = value        
-        maxValues.length = 0        // clear original array
-        maxValues.push(key)         // add the key to the array
+// Using 2 loops and extra variables
+const findMaxRepetitionsOne = (arr) => {
+    if (arr.length === 0) {
+        return null
     }
-    else if (value === max) {       // multiple key with max repetitions
-        maxValues.push(key)
+
+    const counters = {}
+    arr.forEach(item => {
+        // add 1 if item already in counters obj, else set it to 1
+        counters[item] = counters[item] ? counters[item] += 1 : 1
+    })
+
+    let maxRepititions = 0                         // current max repititions
+    const mostRepeated = []                // array contains items with max repetitions
+
+    for (const [key, value] of Object.entries(counters)) {
+        // if value is higher than current max
+        if (value > maxRepititions) {
+            maxRepititions = value
+            mostRepeated.length = 0        // clear original array
+            mostRepeated.push(key)         // add the key to the array
+        }
+        else if (value === maxRepititions) {       // multiple key with max repetitions
+            mostRepeated.push(key)
+        }
     }
+    return { mostRepeated: mostRepeated, repetitions: maxRepititions }
 }
-console.log("Counter: ", counters);
-console.log("Value(s) with most repetitions: ", maxValues, ", repeated ", max, " time(s)");
+
+console.log(findMaxRepetitionsOne(arr_2));
+
+
+// Using only one loop: array.reduce()
+const findMaxRepetitionsTwo = (arr) => {
+    if (arr.length === 0) {
+        return null
+    }
+    const result = arr.reduce((accum, item) => {
+        // count occurences for each item
+        accum.counters[item] = accum.counters[item] + 1 || 1
+
+        // current most repeated items
+        const maxVals = Object.keys(accum.mostRepeated)
+
+        // if NO most repeated item exist (first loop) or their repetitions < those of newly counted item
+        // => there's a new most repeated item, clear the current maxItems
+        if (maxVals.length === 0 || accum.counters[maxVals[0]] < accum.counters[item]) {
+            accum.mostRepeated = {}             // clear obj
+            accum.mostRepeated[item] = true     // add the new max item to obj
+        }
+
+        // if some most repeated item exist and their repetitions === those of newly counted item,
+        // => add that item to maxItems obj
+        else if (maxVals.length && accum.counters[maxVals[0]] === accum.counters[item]) {
+            accum.mostRepeated[item] = true
+        }
+
+        return { ...accum }
+    }, { mostRepeated: {}, counters: {} })
+
+    const mostRepeatedItems = Object.keys(result.mostRepeated)
+    return { mostRepeated: mostRepeatedItems, repititions: result.counters[mostRepeatedItems[0]] }
+}
+
+console.log(findMaxRepetitionsTwo(arr_2));
